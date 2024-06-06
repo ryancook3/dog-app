d8254730347a29f78e8b903d565d4302198a7943
RCook-OTA <rcook@opentechalliance.com>
2024-06-05 20:47:39 -0700

    add Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // We'll create this file next
+import './Navbar.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <Link to="/">Logo</Link>
       </div>
-      <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/register">Register</Link></li>
-        <li><Link to="/upload">Memory Upload</Link></li>
-        <li><Link to="/browse">Browse</Link></li>
+      <button className="navbar-toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+      <ul className={`navbar-links ${isOpen ? 'open' : ''}`}>
+        <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
+        <li><Link to="/about" onClick={toggleMenu}>About</Link></li>
+        <li><Link to="/login" onClick={toggleMenu}>Login</Link></li>
+        <li><Link to="/register" onClick={toggleMenu}>Register</Link></li>
+        <li><Link to="/upload" onClick={toggleMenu}>Memory Upload</Link></li>
+        <li><Link to="/browse" onClick={toggleMenu}>Browse</Link></li>
       </ul>
     </nav>
   );
